28d4c1f7ab923676c1e6624baf2b571b79a171a0
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-06-22 12:13:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <assert.h>
 #include "initialize.h"
 
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
